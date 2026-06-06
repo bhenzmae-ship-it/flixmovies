47b9725a5f9a2dfa38fc450441ed11a1a73e2454
bhenzmae-ship-it <bhenzmae@gmail.com>
2026-06-07 02:28:23 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'const API_KEY = 'db8e12064a5542eb774a36a5378e52c6';
+const API_KEY = 'const API_KEY = '2268fa0c2259755d6b452415b8ebdce7;
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMG_URL = 'https://image.tmdb.org/t/p/original';
     let currentItem;
